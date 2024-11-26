602607da22c1adf976480d446a421cbb52bd2398
kauadrar <kauadrar@gmail.com>
2024-11-25 21:19:42 -0300

    feat: create get_all_genres method

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -7,10 +7,9 @@ class TmdbService
   end
 
   def discover_movies
-    language = Language.find_by(code: "pt-BR")
+    language = Language.find_or_create_by(code: "pt-BR", name: "Português (Brasil)")
 
     response = @tmdb_api["/discover/movie?&language=#{language.code}"].get
-    JSON.parse(response.body)
 
     tmdb_movies = JSON.parse(response.body)["results"]
 
@@ -37,6 +36,33 @@ class TmdbService
         name: tmdb_movie["title"],
         poster_path: tmdb_movie["poster_path"],
         overview: tmdb_movie["overview"],
+      )
+    end
+  end
+
+  def get_all_genres
+    language = Language.find_or_create_by(code: "pt-BR", name: "Português (Brasil)")
+    puts "language: #{language}"
+
+    tmdb_movies_genres_response = @tmdb_api["/genre/movie/list?language=#{language.code}"].get
+
+    tmdb_movies_genres = JSON.parse(tmdb_movies_genres_response.body)["genres"]
+
+    tmdb_shows_genres_response = @tmdb_api["/genre/tv/list?language=#{language.code}"].get
+
+    tmdb_shows_genres = JSON.parse(tmdb_shows_genres_response.body)["genres"]
+
+    all_genres = tmdb_movies_genres + tmdb_shows_genres
+
+    all_genres.each do |tmdb_genre|
+      next if Genre.exists?(tmdb_id: tmdb_genre["id"])
+
+      genre = Genre.create(tmdb_id: tmdb_genre["id"])
+
+      GenreTranslation.create(
+        genre: genre,
+        language: language,
+        name: tmdb_genre["name"],
       )
     end
   end
