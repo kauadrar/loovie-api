25273f44e9ce63d23e51d4a097ea164cfc68c7a0
kauadrar <kauadrar@gmail.com>
2024-11-27 19:23:29 -0300

    feat: create discover_shows method

--- app/services/tmdb_service.rb
+++ app/services/tmdb_service.rb
@@ -9,14 +9,14 @@ class TmdbService
   def discover_movies
     language = Language.find_or_create_by(code: "pt-BR", name: "Português (Brasil)")
 
-    response = @tmdb_api["/discover/movie?&language=#{language.code}"].get
+    response = @tmdb_api["/discover/movie?language=#{language.code}"].get
 
     tmdb_movies = JSON.parse(response.body)["results"]
 
     tmdb_movies.each do |tmdb_movie|
       tmdb_id = tmdb_movie["id"]
 
-      return if Movie.exists?(tmdb_id: tmdb_id)
+      next if Movie.exists?(tmdb_id: tmdb_id)
 
       movie = Movie.create(
         tmdb_id:,
@@ -48,6 +48,52 @@ class TmdbService
         TitleGenre.create(
           genre: genre,
           title: movie,
+        )
+      end
+    end
+  end
+
+  def discover_shows
+    language = Language.find_or_create_by(code: "pt-BR", name: "Português (Brasil)")
+
+    response = @tmdb_api["/discover/tv?language=#{language.code}"].get
+
+    tmdb_shows = JSON.parse(response.body)["results"]
+
+    tmdb_shows.each do |tmdb_show|
+      tmdb_id = tmdb_show["id"]
+
+      next if Show.exists?(tmdb_id: tmdb_id)
+
+      show = Show.create(
+        tmdb_id:,
+        original_name: tmdb_show["original_name"],
+        original_language: tmdb_show["original_language"],
+        backdrop_path: tmdb_show["backdrop_path"],
+        release_date: Date.parse(tmdb_show["first_air_date"]),
+        vote_average: tmdb_show["vote_average"],
+        vote_count: tmdb_show["vote_count"],
+        popularity: tmdb_show["popularity"],
+      )
+
+      ProductionTranslation.create(
+        production: show,
+        language: language,
+        name: tmdb_show["name"],
+        poster_path: tmdb_show["poster_path"],
+        overview: tmdb_show["overview"],
+      )
+
+      genre_ids = tmdb_show["genre_ids"]
+
+      genre_ids.each do |genre_id|
+        genre = Genre.find_by(tmdb_id: genre_id)
+
+        next unless genre
+
+        TitleGenre.create(
+          genre: genre,
+          title: show,
         )
       end
     end
