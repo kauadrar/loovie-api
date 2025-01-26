eec7f1150c70e0f5d50739255e0e0d8404c9c3b4
kaua-drar <kauadrar@gmail.com>
2025-01-26 02:08:24 -0300

    feat: use multisearch job to find more titles if they're missing

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -3,7 +3,11 @@ class TitlesController < ApplicationController
     query = params[:query].present? ? params[:query] : "*"
     language_code = request.headers["accept-language"]
 
-    @titles = Searchkick.search(query, models: [ MovieTranslation, ShowTranslation ], fields: [ "name^10", "overview" ], where: { language_code: })
+    @titles = Searchkick.search(query, models: [ MovieTranslation, ShowTranslation ], fields: [ "name^10", "overview" ], where: { language_code: }, limit: 20)
+
+    if @titles.count < 4
+      MultisearchJob.perform_async(query)
+    end
 
     render "titles/index"
   end
